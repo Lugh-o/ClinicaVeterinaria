2bf20b1ab992bf4233b89af8d5fe32d56ed4abb7
Ramon Luiz <ramonluiz0510@gmail.com>
2025-11-16 15:01:29 -0300

    implementação classe proprietario/proprietarioDAO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import dao.AnimalDAO;
+import dao.ProprietarioDAO;
 import model.Animal;
+import model.Proprietario;
 
 import java.time.LocalDate;
 
@@ -7,12 +9,29 @@ public class Main {
     public static void main(String[] args) {
 
         AnimalDAO animalDao = new AnimalDAO();
+        ProprietarioDAO propDAO = new ProprietarioDAO();
 
         System.out.println(animalDao.getAll());
 
         Animal a = new Animal("cachorro", "cachorro", "cachorro", LocalDate.of(2025, 10, 10), 20.0, 1);
 
 //        System.out.println(animalDao.create(a));
+        
+        Proprietario proprietario = new Proprietario("Adalton", "04117734599", "a@gmail.com", "719177-1512");
+        
+        //System.out.println(propDAO.create(proprietario));
+      
+        /*for(Proprietario prop : propDAO.getAll()) {
+        	System.out.println(prop);
+        	System.out.println();
+        }*/
+        
+        //System.out.println(propDAO.getById(1));
+        
+       System.out.println(propDAO.update(1, proprietario));
+        
+        
+        
 
 
 
--- src/model/Proprietario.java
+++ src/model/Proprietario.java
@@ -1,4 +1,111 @@
 package model;
 
 public class Proprietario {
+	
+	private int id;
+	private int id_telefone;
+	private String nome;
+	private String  cpf;
+	private String email;
+	private String telefone;
+	
+	public Proprietario(int id, String nome, String cpf, String email, int id_telefone, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.id_telefone = id_telefone;
+		this.telefone = telefone;
+	}
+	
+	public Proprietario(int id, String nome, String cpf, String email, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefone = telefone;
+	}
+	
+	public Proprietario(String nome, String cpf, String email, String telefone) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefone = telefone;
+	}
+	
+	public Proprietario(int id, String nome, String cpf, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+	
+	public Proprietario(String nome, String cpf, String email) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_telefone() {
+		return id_telefone;
+	}
+
+	public void setId_telefone(int id_telefone) {
+		this.id_telefone = id_telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String toString() {
+		return "Id: " + id
+				+ "\nNome: " + nome
+				+ "\nCPF: " + cpf
+				+ "\nEmail: " + email
+				+ "\ntelefone: " + telefone;
+	}
+
+	
+	
+	
+
+	
+	
 }
