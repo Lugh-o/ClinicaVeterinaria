bc8b7ca508cf1da7858138d63ca506ec16a12fff
lu <138606946+luixgustavooo@users.noreply.github.com>
2025-11-22 22:42:26 -0300

    Add Veterinario class with attributes and methods

---
+++ src/model/Veterinario.java
@@ -0,0 +1,80 @@
+package model;
+
+public class Veterinario {
+
+    private int id;
+    private String nome;
+    private String crmv;
+    private int idEspecialidade;
+    private String especialidade;
+    private int idTelefone;
+    private String telefone;
+
+    public Veterinario(int id, String nome, String crmv, int idEspecialidade, String especialidade, int idTelefone, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.crmv = crmv;
+        this.idEspecialidade = idEspecialidade;
+        this.especialidade = especialidade;
+        this.idTelefone = idTelefone;
+        this.telefone = telefone;
+    }
+
+    public Veterinario() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrmv() {
+        return crmv;
+    }
+
+    public void setCrmv(String crmv) {
+        this.crmv = crmv;
+    }
+
+    public int getIdEspecialidade() {
+        return idEspecialidade;
+    }
+
+    public void setIdEspecialidade(int idEspecialidade) {
+        this.idEspecialidade = idEspecialidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public int getIdTelefone() {
+        return idTelefone;
+    }
+
+    public void setIdTelefone(int idTelefone) {
+        this.idTelefone = idTelefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
